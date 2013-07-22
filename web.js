418ed52af7e069019b201b25b212ebc96aab6f00
Hemanth.HM <hemanth.hm@gmail.com>
2013-07-22 18:09:44 +0530

    Changed port as per the need

--- web.js
+++ web.js
@@ -5,5 +5,4 @@ http.createServer(function (req, res) {
   var url_parts = url.parse(req.url, true);
   var status = url_parts.pathname.replace(/^\/([^\/]*).*$/, '$1');
   res.end(http.STATUS_CODES[status] || "Undefined status : "+ status);
-}).listen(1337, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:1337/');
+}).listen(process.env.PORT || 5000);
