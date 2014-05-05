e3a3969b9dce5d5933749fb0a74786fa2161e6a1
Hemanth.HM <hemanth.hm@gmail.com>
2014-05-05 10:39:06 +0530

    Better error message

--- web.js
+++ web.js
@@ -4,5 +4,5 @@ http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   var url_parts = url.parse(req.url, true);
   var status = url_parts.pathname.replace(/^\/([^\/]*).*$/, '$1');
-  res.end(http.STATUS_CODES[status] || "Undefined status : "+ status || "Try /404");
+  res.end(http.STATUS_CODES[status] || "Unknown Status Code! Try /404 in the (c)URL");
 }).listen(process.env.PORT || 5000);
